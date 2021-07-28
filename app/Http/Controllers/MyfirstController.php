2822305cfad2146afb9cfeb73ce3cab964e2b518
Jemaline <temariojemaline56@gmail.com>
2021-07-28 13:34:48 +0800

    Sample page title

--- app/Http/Controllers/MyfirstController.php
+++ app/Http/Controllers/MyfirstController.php
@@ -9,7 +9,9 @@ class MyfirstController extends Controller
     public function index()
     {
         $sum= 5+2;
-        return view('resultpage',['result'=> $sum]);
+        return view('resultpage',['result'=> $sum,
+        'title'=> "Sample page title"]);
+
     }
 
 }
--- resources/views/resultpage.blade.php
+++ resources/views/resultpage.blade.php
@@ -2,10 +2,10 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>The result's page</title>
+	<title>{{ $title}}</title>
 </head>
 <body>
-<h1>The result is:{{ $result}}</h1>
+<h1>The result is: {{ $result}}</h1>
 
 </body>
 </html>
