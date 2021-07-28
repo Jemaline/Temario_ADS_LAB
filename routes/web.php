4d721702c0aa3b358201641938be075723571e01
Jemaline <temariojemaline56@gmail.com>
2021-07-28 13:58:38 +0800

    Printed the sum of 5 and 8 is 13

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-{
-    Route::get('/displaythesum', 'MyfirstController@index');
-}
+
+    Route::get('/displaythesum', 'MyfirstController@index');{
+
+    };
+    Route::get ('/addition',function(){
+    $num1 =5;
+    $num2 =8;
+    $result =$num1 + $num2;
+    return " The sum of ".$num1."and".$num2."is ".$result;
+
+});
 
